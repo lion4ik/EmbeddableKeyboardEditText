5d85ee6c366839a531ded44ac3e5d520c0042b96
Alexey Pushkarev1 <alexey_pushkarev1@epam.com>
2016-08-02 11:58:09 +0300

    rename sample module name

---
+++ embeddablekeyboard/src/main/java/ru/alexey/lion4ik/EmbeddableKeyboard.java
@@ -0,0 +1,13 @@
+package ru.alexey.lion4ik;
+
+/**
+ * Created by Alexey_Pushkarev1 on 07/12/2016.
+ */
+public interface EmbeddableKeyboard {
+
+    boolean isKeyboardShown();
+
+    void showKeyboard();
+
+    void hideKeyboard();
+}
